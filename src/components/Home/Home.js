d4f706e320583caab8640ac750e3cc0bfeb31602
Nathan McCoy <nathanmccoy25@gmail.com>
2017-06-09 00:38:34 -0600

    added ux errors and changed body height to update depending on amount of todos

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -14,28 +14,72 @@ class Home extends Component {
     }
 
     addTodo = (task) => {
-      console.log(task)
-      document.getElementById('task-input').value = ''
-      this.setState({
-        todos: this.state.todos.concat({task: task, completed: false, id: this.state.id++})
-      })
+        document.getElementById('task-input').value = ''
+        var tasks = this.state.todos
+        var duplicate = false
+        //targets the error div to change styling later
+        var error = document.getElementById('error-wrap')
+
+        //check tasks for duplicates
+        if (tasks.length) {
+            for (var i = 0; i < tasks.length; i++) {
+                if (tasks[i].task === task) {
+                    duplicate = true
+                }
+            }
+        }
+        if (task && !duplicate) {
+          document.getElementsByTagName('form')[0].style.marginBottom = '15px'
+          error.style.height = '0px'
+          error.style.visibility = 'hidden'
+            this.setState({
+                todos: this.state.todos.concat({
+                    task: task,
+                    completed: false,
+                    id: this.state.id++
+                })
+            })
+        }
+        else if(duplicate){
+          document.getElementsByTagName('form')[0].style.marginBottom = '0px'
+          error.innerHTML = 'That todo already exists'
+          error.style.height = '20px'
+          error.style.visibility = 'visible'
+        }
+        else{
+          document.getElementsByTagName('form')[0].style.marginBottom = '0px'
+          error.innerHTML = 'field required'
+          error.style.height = '20px'
+          error.style.visibility = 'visible'
+        }
     }
 
     removeTodo = (id) => {
-      this.setState({
-        todos: this.state.todos.filter((obj) => {
-          if(obj.id === id){
-            return false
-          }
-          return true
+        this.setState({
+            todos: this.state.todos.filter((obj) => {
+                if (obj.id === id) {
+                    return false
+                }
+                return true
+            })
         })
-      })
     }
 
     render() {
-      var todos = this.state.todos.map(obj => {
-        return <Task  info={{task: obj.task, completed: obj.completed, id: obj.id}} key={obj.id} remove={this.removeTodo}/>
-      })
+        var todos = this.state.todos.map(obj => {
+            return <Task info={{
+                task: obj.task,
+                completed: obj.completed,
+                id: obj.id
+            }} key={obj.id} remove={this.removeTodo}/>
+        })
+        //adjusts page height
+        if(todos.length > 9){
+          document.getElementsByTagName('body')[0].style.height = 'auto'
+        }
+        else{
+          document.getElementsByTagName('body')[0].style.height = '100vh'
+        }
         return (
             <div id="home-wrap">
                 <nav>
@@ -43,8 +87,11 @@ class Home extends Component {
                 </nav>
                 <form>
                     <input id="task-input" placeholder="new todo"/>
-                    <div id="add-button" onClick={() => {this.addTodo(document.getElementById('task-input').value)}}>+</div>
+                    <div id="add-button" onClick={() => {
+                        this.addTodo(document.getElementById('task-input').value)
+                    }}>+</div>
                 </form>
+                <div id="error-wrap"></div>
                 {todos}
             </div>
         )
